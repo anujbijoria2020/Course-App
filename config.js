c7defa188080a247edf8bfa2da66243c80962022
Anuj patel <165664773+anujbijoria2020@users.noreply.github.com>
2025-05-18 20:53:25 +0530

    Create config.js
    configurations

---
+++ config.js
@@ -0,0 +1,9 @@
+
+const JWT_ADMIN_SECRET_KEY = process.env.JWT_ADMIN_SECRET_KEY;
+
+const JWT_USER_SECRET_KEY = process.env.JWT_USER_SECRET_KEY;
+
+module.exports = {
+    JWT_ADMIN_SECRET_KEY,
+    JWT_USER_SECRET_KEY
+}
